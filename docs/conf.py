f7bd7ed4728687c862ed13e0009acf289456c69f
Surya <surya.thiru001@gmail.com>
2020-12-13 09:42:10 +0530

    add setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
+
 sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "mlgauge")
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'MLgauge'
-copyright = '2020, Surya Krishnamurthy'
-author = 'Surya Krishnamurthy'
+project = "MLgauge"
+copyright = "2020, Surya Krishnamurthy"
+author = "Surya Krishnamurthy"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,22 +28,17 @@ author = 'Surya Krishnamurthy'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-autodoc_default_options = {
-    'special-members': '__init__'
-}
+autodoc_default_options = {"special-members": "__init__"}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -50,9 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxawesome_theme'
+html_theme = "sphinxawesome_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent.resolve()
+package_dir = here / "mlgauge"
+
+# Packages required for this module to be executed
+def list_reqs(fname="requirements.txt"):
+    with open(fname) as fd:
+        return fd.read().splitlines()
+
+
+# Get the long description from the README file
+long_description = open(here / "README.md").read()
+
+# Version
+version = open(package_dir / "VERSION").read().strip()
+
+setup(
+    name="mlgauge",
+    version=version,
+    description="Library to compare machine learning methods across datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SuryaThiru/mlgauge",
+    author="Surya Krishnamurthy",
+    packages=find_packages(exclude=("tests")),
+    license="License :: OSI Approved :: MIT License",
+    install_requires=list_reqs(),
+    extras_requre={"dev": ["pytest", "black"]},
+    keywords=["machine learning", "benchmark", "pmlb"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities",
+    ],
+)
